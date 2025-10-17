777468befce88fa8e495693dceaa652f6e8d8b28
SpekyLord <trashdump098123@gmail.com>
2025-10-17 16:05:43 +0800

    feat: added power function

--- opertations.py
+++ opertations.py
@@ -1,11 +1,17 @@
 def add(x,y):
     return x + y
+
 def minus(x,y):
     return x - y
+
 def devide(x,y):
     if (x == 0):
         return "Error"
     else:
         return x / y
+    
 def multiply(x,y):
     return x * y
+
+def power(x,y):
+    return x ** y
