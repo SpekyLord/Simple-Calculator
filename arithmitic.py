5162e6cc8e944df6b2d83f60244e61c2666a30dd
SpekyLord <trashdump098123@gmail.com>
2025-10-17 15:26:10 +0800

    addede the needed functions for the calculator

---
+++ arithmitic.py
@@ -0,0 +1,11 @@
+def add(x,y):
+    return x + y
+def minus(x,y):
+    return x-y
+def devide(x,y):
+    if (x == 0 or y == 0):
+        return "Error"
+    else:
+        return x/y
+def mult(x,y):
+    return x*y
